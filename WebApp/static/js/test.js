6d9b6cae181a4cff71031cb37c455d1789564b0a
Krzysztof Cabala <210047@student.pwr.wroc.pl>
2016-04-23 21:15:49 +0200

    tsty jsow

---
+++ WebApp/static/js/test.js
@@ -0,0 +1,7 @@
+$(document).ready( function() {
+	setInterval(function () {
+		$.get("/test_js_func" , function(dane){
+
+    	});
+    },1000);
+});
---
+++ WebApp/tasks/api.py
@@ -0,0 +1,13 @@
+import json
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from .models import Task
+
+@login_required(login_url='/')
+def test_js_func(request):
+	tasks = Task.objects.filter(user=request.user).order_by('-time')
+	response_data={}
+	for task in tasks:
+		response_data['id'] = task.id
+		response_data['value'] = task.cos
+	return HttpResponse(json.dumps(response_data),content_type="application/json")
