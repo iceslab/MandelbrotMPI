73bb4c15c63049901ff1c977daf7896f3be7d6a6
Krzysztof Cabala <210047@student.pwr.wroc.pl>
2016-05-27 00:05:12 +0200

    dummy

--- Cluster/run.sh
+++ Cluster/run.sh
@@ -12,10 +12,11 @@ fi
 
 if make -j8 ; then
 	rsync -av -e "ssh" ./main user@192.168.30.196:~/main
+	rsync -av -e "ssh" ./main user@192.168.30.182:~/main
 	rsync -av -e "ssh" ./main localhost:~/main
 	#rsync -av -e "ssh" ./main user@156.17.41.52:~/main
 	# rsync -av -e "ssh" ./main user@156.17.41.58:~/main
-	# rsync -av -e "ssh" ./main user@156.17.41.53:~/main
+	# rsync -av -e "ssh" ./main user@156.17.41.53:~/main 
 	# rsync -av -e "ssh" ./main user@156.17.41.56:~/main
 	#rsync -av -e "ssh" ./main kejt@192.168.30.194:~/main
 	if [ $# -lt 5 ]; then
